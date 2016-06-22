ee87a9c2903b1f5b60f1a7bfbc7c104bbcebc6ad
vadis <vadis.programmist@mail.ru>
2016-06-22 06:13:04 +0300

    Revert "added method show_rolling_die and choice of amout throws and sum throws"
    This reverts commit 1da1027d9a4641f332b4c718ec725f5f31106fa8.

--- dice.rb
+++ dice.rb
@@ -1,20 +1,9 @@
 #coding: utf-8
 
-def show_rolling_die
-  100.times do
-    print "\r#{rand(6) + 1}"
-    sleep 0.01
-  end
+puts 'Kость вращается!'
+100.times do
+  print "\r #{rand(6) + 1}"
+  sleep 0.05
 end
 
-puts 'Сколько раз вращать кость?'
-number = STDIN.gets.chomp.to_i
-
-sum = 0
-
-number.times {
-  puts 'Kость вращается!'
-  show_rolling_die
-  time_value = nil
-  puts "\nВыпало #{rand(6) + 1}"
-}
+puts "\n Выпало #{rand(6) + 1}"
