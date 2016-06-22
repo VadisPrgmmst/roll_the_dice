0087ee1dbe8981c0ee59e3eb6ad39ace01303d5b
vadis <vadis.programmist@mail.ru>
2016-06-22 05:28:18 +0300

    deleted var dice

--- dice.rb
+++ dice.rb
@@ -1,11 +1,9 @@
 #coding: utf-8
 
-dice = rand(6) + 1
-
 puts 'Kость вращается!'
 100.times do
   print "\r #{rand(6) + 1}"
   sleep 0.05
 end
 
-puts "\n Выпало #{dice}"
+puts "\n Выпало #{rand(6) + 1}"
