1da1027d9a4641f332b4c718ec725f5f31106fa8
vadis <vadis.programmist@mail.ru>
2016-06-22 06:12:37 +0300

    added method show_rolling_die and choice of amout trows

--- dice.rb
+++ dice.rb
@@ -1,9 +1,20 @@
 #coding: utf-8
 
-puts 'Kость вращается!'
-100.times do
-  print "\r #{rand(6) + 1}"
-  sleep 0.05
+def show_rolling_die
+  100.times do
+    print "\r#{rand(6) + 1}"
+    sleep 0.01
+  end
 end
 
-puts "\n Выпало #{rand(6) + 1}"
+puts 'Сколько раз вращать кость?'
+number = STDIN.gets.chomp.to_i
+
+sum = 0
+
+number.times {
+  puts 'Kость вращается!'
+  show_rolling_die
+  time_value = nil
+  puts "\nВыпало #{rand(6) + 1}"
+}
