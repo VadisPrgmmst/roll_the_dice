810759d218812cfcc5d508214f99a80f228d5064
vadis <vadis.programmist@mail.ru>
2016-06-22 07:40:30 +0300

    added file README.md

--- dice.rb
+++ dice.rb
@@ -1,9 +1,23 @@
 #coding: utf-8
 
-puts 'Kость вращается!'
-100.times do
-  print "\r #{rand(6) + 1}"
-  sleep 0.05
+def show_rolling_die
+  100.times do
+    print "\r#{rand(6) + 1}"
+    sleep 0.01
+  end
 end
 
-puts "\n Выпало #{rand(6) + 1}"
+
+puts 'Сколько раз бросать кость'
+number = STDIN.gets.chomp.to_i
+
+sum = 0
+number.times do
+  puts 'Kость вращается!'
+  show_rolling_die
+  time_value = nil
+  puts "\nВыпало #{time_value = rand(6) + 1}"
+  sum += time_value
+end
+
+puts "\nСумма все выпавших костей #{sum}"
